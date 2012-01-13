9d1dd5ac24cf5e356fd37b04f8012717d61dd289
Kautuk Consul <consul.kautuk@gmail.com>
2012-01-12 17:20:08 -0800

    mm/vmalloc.c: eliminate extra loop in pcpu_get_vm_areas error path
    If either of the vas or vms arrays are not properly kzalloced, then the
    code jumps to the err_free label.
    
    The err_free label runs a loop to check and free each of the array members
    of the vas and vms arrays which is not required for this situation as none
    of the array members have been allocated till this point.
    
    Eliminate the extra loop we have to go through by introducing a new label
    err_free2 and then jumping to it.
    
    [akpm@linux-foundation.org: remove now-unneeded tests]
    Signed-off-by: Kautuk Consul <consul.kautuk@gmail.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -2491,10 +2491,8 @@ found:
 
 err_free:
 	for (area = 0; area < nr_vms; area++) {
-		if (vas)
-			kfree(vas[area]);
-		if (vms)
-			kfree(vms[area]);
+		kfree(vas[area]);
+		kfree(vms[area]);
 	}
 	kfree(vas);
 	kfree(vms);
