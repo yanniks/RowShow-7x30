853ff5d11e0ef62753d3056dc3fef5841a03a5e1
Kuogee Hsieh <khsieh@codeaurora.org>
2012-09-13 13:22:04 -0700

    msm_fb: display: add pipe commit ioctl (Adapted)
    currently, pan display serves as pipe commit. However it
    may spend long time pending on cosole_lock() which cause
    flickering on HDMI. Add pipe commit ioctl to allow commit
    from overlay in addition to pan_display.
    
    CRs-fixed: 391564
    Change-Id: I163b5d96f727b70b4d5693e9a414b59339818998
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    
    Adapted for htc msm7x30 (primou) base by @synergydev

--- drivers/video/msm/mdp4_overlay_dsi_cmd.c
+++ drivers/video/msm/mdp4_overlay_dsi_cmd.c
@@ -250,7 +250,7 @@ void mdp4_dsi_cmd_pipe_queue(int cndx, struct mdp4_overlay_pipe *pipe)
 
 static void mdp4_dsi_cmd_blt_ov_update(struct mdp4_overlay_pipe *pipe);
 
-int mdp4_dsi_cmd_pipe_commit(void)
+int mdp4_dsi_cmd_pipe_commit(int cndx, int wait)
 {
 	int  i, undx;
 	int mixer = 0;
@@ -376,6 +376,12 @@ int mdp4_dsi_cmd_pipe_commit(void)
 	spin_unlock_irqrestore(&vctrl->spin_lock, flags);
 
 	mdp4_stat.overlay_commit[pipe->mixer_num]++;
+
+	if (wait) {
+		long long tick;
+
+		mdp4_dsi_cmd_wait4vsync(cndx, &tick);
+	}
 
 	return cnt;
 }
@@ -1109,6 +1115,7 @@ void mdp4_dsi_cmd_overlay(struct msm_fb_data_type *mfd)
 	struct mdp4_overlay_pipe *pipe;
 	long long xx;
 	unsigned long flags;
+	long long tick;
 
 	vctrl = &vsync_ctrl_db[cndx];
 
@@ -1131,12 +1138,12 @@ void mdp4_dsi_cmd_overlay(struct msm_fb_data_type *mfd)
 	mdp4_overlay_mdp_perf_upd(mfd, 1);
 
 	mutex_lock(&mfd->dma->ov_mutex);
-	mdp4_dsi_cmd_pipe_commit();
+	mdp4_dsi_cmd_pipe_commit(cndx, 0);
 	mutex_unlock(&mfd->dma->ov_mutex);
 
-	mdp4_dsi_cmd_wait4vsync(0, &xx);
 	vctrl->expire_tick = VSYNC_EXPIRE_TICK;
 	vctrl->clk_control = 1;
+	mdp4_dsi_cmd_wait4vsync(cndx, &tick);
 
 	mdp4_overlay_mdp_perf_upd(mfd, 0);
 }
