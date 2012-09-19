96f34b5a17e989f1646256f8d34441926bb9dce9
Jay W <cn.fyodor@gmail.com>
2012-09-19 14:37:20 +0800

    vision: cleanup MSM7KV2_1X_AUDIO configs

--- arch/arm/mach-msm/board-vision.c
+++ arch/arm/mach-msm/board-vision.c
@@ -150,7 +150,7 @@ unsigned int vision_get_engineerid(void)
 	return engineerid;
 }
 
-#if defined(CONFIG_MSM7KV2_1X_AUDIO) || defined(CONFIG_MSM7KV2_AUDIO)
+#ifdef CONFIG_MSM7KV2_AUDIO
 static struct resource msm_aictl_resources[] = {
 	{
 		.name = "aictl",
@@ -1456,7 +1456,7 @@ static void __init vision_init_marimba(void)
 	}
 }
 
-#if defined(CONFIG_MSM7KV2_1X_AUDIO) || defined(CONFIG_MSM7KV2_AUDIO)
+#ifdef CONFIG_MSM7KV2_AUDIO
 #define DEC0_FORMAT ((1<<MSM_ADSP_CODEC_MP3)| \
 	(1<<MSM_ADSP_CODEC_AAC)|(1<<MSM_ADSP_CODEC_WMA)| \
 	(1<<MSM_ADSP_CODEC_WMAPRO)|(1<<MSM_ADSP_CODEC_AMRWB)| \
@@ -3077,7 +3077,7 @@ static struct platform_device *devices[] __initdata = {
 #ifdef CONFIG_INPUT_CAPELLA_CM3602
         &capella_cm3602,
 #endif
-#if defined(CONFIG_MSM7KV2_1X_AUDIO) || defined(CONFIG_MSM7KV2_AUDIO)
+#ifdef CONFIG_MSM7KV2_AUDIO
         &msm_aictl_device,
         &msm_mi2s_device,
         &msm_lpa_device,
@@ -3223,7 +3223,7 @@ static void __init vision_init(void)
 #endif
 	qup_device_i2c_init();
 	vision_init_marimba();
-#if defined(CONFIG_MSM7KV2_1X_AUDIO) || defined(CONFIG_MSM7KV2_AUDIO)
+#ifdef CONFIG_MSM7KV2_AUDIO
 	aux_pcm_gpio_init();
 	msm_snddev_init();
 	audience_gpio_init();
