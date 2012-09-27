d4d688d0f7d859be189dc4de407926a0a9a72e0e
Siddhartha Agrawal <agrawals@codeaurora.org>
2012-09-27 16:12:35 -0700

    msm_fb: display: Timeout pan display if time greater than 100ms
    Timeout if pan display is taking more than 100ms to complete. This
    will unblock the userspace thread and continue. This will solve
    the hang issues.
    
    Change-Id: Ica6a395b0e992263567633d6dc93a92ee1fa59cf
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>
    (cherry picked from commit 7c270369ccb7eca4934d6005b1960ac7ec2ed2fc)

--- drivers/video/msm/mipi_dsi.c
+++ drivers/video/msm/mipi_dsi.c
@@ -69,6 +69,8 @@ static int mipi_dsi_off(struct platform_device *pdev)
 	struct msm_fb_data_type *mfd;
 	struct msm_panel_info *pinfo;
 
+	pr_debug("Start of %s....:\n", __func__);
+
 	mfd = platform_get_drvdata(pdev);
 	pinfo = &mfd->panel_info;
 
@@ -121,7 +123,7 @@ static int mipi_dsi_off(struct platform_device *pdev)
 	else
 		up(&mfd->dma->mutex);
 
-	pr_debug("%s-:\n", __func__);
+	pr_debug("End of %s ....:\n", __func__);
 
 	return ret;
 }
@@ -139,6 +141,8 @@ static int mipi_dsi_on(struct platform_device *pdev)
 	u32 ystride, bpp, data;
 	u32 dummy_xres, dummy_yres;
 	int target_type = 0;
+
+	pr_debug("Start of %s:....\n", __func__);
 
 	mfd = platform_get_drvdata(pdev);
 	fbi = mfd->fbi;
@@ -312,7 +316,7 @@ static int mipi_dsi_on(struct platform_device *pdev)
 	else
 		up(&mfd->dma->mutex);
 
-	pr_debug("%s-:\n", __func__);
+	pr_debug("End of %s....:\n", __func__);
 
 	return ret;
 }
