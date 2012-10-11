ac21486d3f201c25da31b1877b017e320e48d956
Jay W <cn.fyodor@gmail.com>
2012-10-11 13:43:28 +0800

    msm_fb: mddi: fix build error caused by the last patch

--- drivers/video/msm/mdp.h
+++ drivers/video/msm/mdp.h
@@ -803,14 +803,6 @@ static inline int mdp4_mddi_on(struct platform_device *pdev)
 #endif
 
 
-#ifndef CONFIG_FB_MSM_MDDI
-static inline void mdp4_mddi_rdptr_init(int cndx)
-{
-	/* empty */
-}
-
-#endif
-
 void set_cont_splashScreen_status(int);
 
 int mdp_hw_cursor_update(struct fb_info *info, struct fb_cursor *cursor);
