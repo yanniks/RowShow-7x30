5ddcf0d95a955b112156ae89172c7e636ae74c14
Shruthi Krishna <skrish@codeaurora.org>
2012-12-17 15:09:59 -0800

    msm_fb: display: add wfd clk ctrl in pipe_commit
    mdp_clk_ctrl on is not called when commit is from overlay commit path
    which can cause mdp hang. mdp_clk_ctrl off should be after mdp
    overlay2 is done, so schedule a work in overlay2 done isr.
    
    Change-Id: I94fd08da27cfda114f5cc0056a5103f0a9b772f6
    Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>

--- drivers/video/msm/mdp.c
+++ drivers/video/msm/mdp.c
@@ -1588,7 +1588,9 @@ void mdp_clk_ctrl(int on)
 			mdp_clk_cnt--;
 			if (mdp_clk_cnt == 0)
 				mdp_clk_disable_unprepare();
-		}
+		} else
+			pr_err("%s: %d: mdp clk off is invalid\n",
+			       __func__, __LINE__);
 	}
 	mutex_unlock(&mdp_suspend_mutex);
 }
