8a4a8da18fa8d923fd5e0ea569c9eb59bc673722
Jay Wu <cn.fyodor@gmail.com>
2012-12-18 00:14:31 +0800

    vision: memory layout reworked

--- arch/arm/mach-msm/include/mach/vmalloc.h
+++ arch/arm/mach-msm/include/mach/vmalloc.h
@@ -20,17 +20,7 @@
 #ifdef CONFIG_VMSPLIT_2G
 #define VMALLOC_END	  (PAGE_OFFSET + 0x7A000000)
 #else
-#ifdef CONFIG_ARCH_MSM8X60
-#define VMALLOC_END       (PAGE_OFFSET + 0x3E000000)
-#elif defined(CONFIG_ARCH_MSM7X30)
-#ifdef CONFIG_DEBUG_LL
-#define VMALLOC_END	  (PAGE_OFFSET + 0x3D800000)
-#else
-#define VMALLOC_END	  (PAGE_OFFSET + 0x3E000000)
-#endif
-#else
 #define VMALLOC_END	  (PAGE_OFFSET + 0x3A000000)
-#endif
 #endif
 
 #endif
