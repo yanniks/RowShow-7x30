c6fde735370e2a48e2b31129da24af90d97c0055
Carl Vanderlip <carlv@codeaurora.org>
2012-12-17 20:23:18 -0800

    video: msm: Finish histogram collection framework for MDP blocks
    The patch "Add Histogram collection framework for MDP blocks" was included
    on this branch; however, it was an incomplete version. The additions are
    more histogram function pointers and better comment formatting.
    
    Change-Id: Id5fb20f07f2c50eb678fd8f57238f4ef3d77acb0
    Signed-off-by: Carl Vanderlip <carlv@codeaurora.org>
    (cherry picked from commit 7b8b64075ee4b61c15e28f3042672b26674e0b45)

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -3928,7 +3928,7 @@ static int msm_fb_ioctl(struct fb_info *info, unsigned int cmd,
 			goto msm_fb_ioctl_exit;
 		}
 
-		if (!mfd->do_histogram) {
+		if (!mfd->start_histogram) {
 			ret = -ENODEV;
 			goto msm_fb_ioctl_exit;
 		}
@@ -3955,11 +3955,11 @@ static int msm_fb_ioctl(struct fb_info *info, unsigned int cmd,
 		if (ret)
 			goto msm_fb_ioctl_exit;
 
-		ret = mdp_histogram_start(&hist_req);
+		ret = mfd->start_histogram(&hist_req);
 		break;
 
 	case MSMFB_HISTOGRAM_STOP:
-		if (!mfd->do_histogram) {
+		if (!mfd->stop_histogram) {
 			ret = -ENODEV;
 			goto msm_fb_ioctl_exit;
 		}
@@ -3968,7 +3968,7 @@ static int msm_fb_ioctl(struct fb_info *info, unsigned int cmd,
 		if (ret)
 			goto msm_fb_ioctl_exit;
 
-		ret = mdp_histogram_stop(info, block);
+		ret = mfd->stop_histogram(info, block);
 		break;
 
 
