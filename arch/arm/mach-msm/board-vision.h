25e8cbca4081d882dd7be07f32f22af96cb17d04
Jay Wu <cn.fyodor@gmail.com>
2012-12-28 13:31:01 +0800

    vision: resize SF_size again and restore the stock value of VMALLOC_END

--- arch/arm/mach-msm/board-vision.h
+++ arch/arm/mach-msm/board-vision.h
@@ -53,11 +53,7 @@ extern struct platform_device msm_device_mddi0;
 
 #define PMEM_KERNEL_EBI1_SIZE   0x00600000
 
-#ifdef CONFIG_FB_MSM_DEFAULT_DEPTH_RGB565
-#define MSM_PMEM_SF_SIZE	0x0C00000
-#else
-#define MSM_PMEM_SF_SIZE	0x1A00000
-#endif
+#define MSM_PMEM_SF_SIZE	0x1C00000
 
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MSM_FB_SIZE	0x465000
