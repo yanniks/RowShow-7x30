18d9d17ac1f99c157652bb6e55d5482df050f0b4
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2013-10-13 11:38:57 +0000

    missing stuff from msm_mdp.h
    Change-Id: I1f83098450e70d89a669add06a61bfb567bcfc85

--- include/linux/msm_mdp.h
+++ include/linux/msm_mdp.h
@@ -346,6 +346,14 @@ struct mdp_overlay_pp_params {
 	struct mdp_sharp_cfg sharp_cfg;
 };
 
+enum {
+	BLEND_OP_NOT_DEFINED = 0,
+	BLEND_OP_OPAQUE,
+	BLEND_OP_PREMULTIPLIED,
+	BLEND_OP_COVERAGE,
+	BLEND_OP_MAX,
+};
+
 struct mdp_overlay {
 	struct msmfb_img src;
 	struct mdp_rect src_rect;
@@ -354,6 +362,7 @@ struct mdp_overlay {
 	uint32_t is_fg;		/* control alpha & transp */
 	uint32_t alpha;
 	uint32_t transp_mask;
+	uint32_t blend_op;
 	uint32_t flags;
 	uint32_t id;
 	uint32_t user_data[8];
@@ -534,6 +543,7 @@ struct mdp_buf_sync {
 	uint32_t acq_fen_fd_cnt;
 	int *acq_fen_fd;
 	int *rel_fen_fd;
+	int *retire_fen_fd;
 };
 
 struct mdp_blend_cfg {
