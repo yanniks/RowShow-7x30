026eca8a9ce7f879b3acf6d90b87dac7846a9da6
yanniks <kontakt@yanniks.de>
2013-10-20 18:14:45 +0200

    add RowShow-typical files

---
+++ build_kernel.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+function check_result {
+  if [ "0" -ne "$?" ]
+  then
+    echo $1
+    exit 1
+  fi
+}
+
+if [ -z "$DEFCONFIG" ]
+then
+  if [ -z "$1" ]
+  then
+    echo DEFCONFIG not specified
+    exit 1
+  fi
+  export DEFCONFIG=$1
+fi
+
+echo cloning AnyKernel...
+rm -rf ~/AnyKernel
+git clone git://github.com/yanniks/AnyKernel.git ~/AnyKernel
+echo done!
+echo exporting some functions...
+export ARCH=arm
+echo done!
+echo cleaning...
+make clean
+check_result "Cleaning failed."
+echo done!
+echo start compiling for $DEFCONFIG ...
+make $DEFCONFIG_defconfig
+check_result "Setting defconfig failed."
+make
+check_result "Build failed."
+echo done!
+echo creating update zip...
+bash zip
+check_result "Creating zip failed."
+echo done!
+echo ""
+if [ "$RELEASE" = "true" ]
+then
+  echo releasing...
+  mv ~/kernel-*.zip /var/www/kernel
+  echo released new kernel binary: http://yauniks.dynvpn.de/$ZIPFILENAME
+  check_result "Release failed."
+else
+  echo everything done!
+fi
---
+++ make_defconfig.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+DEFCONFIG_FILE=$1
+LOADMENU=$2
+
+if [ -z "$DEFCONFIG_FILE" ]; then
+	echo "Need defconfig file(spade_defconfig)!"
+	exit -1
+fi
+
+if [ ! -e arch/arm/configs/$DEFCONFIG_FILE ]; then
+	echo "No such file : arch/arm/configs/$DEFCONFIG_FILE"
+	exit -1
+fi
+
+# update current config
+export ARCH=arm
+make $DEFCONFIG_FILE
+
+  if [ "$LOADMENU" = "updateonly" ]
+  then
+    echo defconfig done!
+  else
+    # run menuconfig
+    make menuconfig
+    echo defconfig done!
+  fi
+
+# copy .config to defconfig
+rm arch/arm/configs/${DEFCONFIG_FILE}
+mv .config arch/arm/configs/${DEFCONFIG_FILE}
+# clean kernel object
+make mrproper
